e42902694d47cc4b96816ad26c618e718c22e636
Jeff <jeff@cogentcoder.com>
2020-12-07 23:45:12 -0500

    Changes  to use Bootstrap 5
    Added .prettierrc.json and updated .gitignore

--- src/components/About.js
+++ src/components/About.js
@@ -1,54 +1,33 @@
-import { Component, Fragment } from "react";
+import { Component, Fragment } from 'react'
 
 export class About extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <Fragment>
-        {/* About */}
-        <section id="about">
-          <div className="row">
-            <div className="three columns">
-              <img className="profile-pic" src="images/profilepic.jpg" alt="" />
-            </div>
-            <div className="nine columns main-col">
-              <h2>About Me</h2>
-              <h3 className="rolecontent">
-                I am an <span className="rolename">{resumeData.role} </span>
-                {resumeData.roleDescription}.
-              </h3>
-              <hr />
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    <span>{resumeData.name}</span>
-                    <br />
-                    <span>
-                      {resumeData.address}
-                      <br />
-                      {resumeData.city}, {resumeData.state} {resumeData.zip} US
-                    </span>
-                    <br />
-                    <span>{resumeData.email}</span>
-                  </p>
-                </div>
-                <div className="columns download">
-                  <p>
-                    <a href="#" className="button">
-                      <i className="fa fa-download" />
-                      Download Resume
-                    </a>
-                  </p>
-                </div>
-              </div>{" "}
-              {/* end row */}
-            </div>{" "}
-            {/* end .main-col */}
-          </div>
-        </section>{" "}
-        {/* About End*/}
-      </Fragment>
-    );
-  }
+    render() {
+        let resumeData = this.props.resumeData
+        return (
+            <Fragment>
+                {/* About */}
+                <section id="about" className="py-5 text-center container">
+                    <div className="row py-lg-5">
+                        <div className="col-lg-6 col-md-8 mx-auto">
+                            <h1 className="fw-light">About Me</h1>
+                            <p className="lead">
+                                Hello, I am an{' '}
+                                <span className="rolename">
+                                    {resumeData.role}{' '}
+                                </span>
+                                {resumeData.roleDescription}.
+                            </p>
+                            <p>
+                                <a href="#" className="btn btn-secondary my-2">
+                                    <i className="fa fa-download" /> Download
+                                    Resume
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </section>{' '}
+                {/* About End*/}
+            </Fragment>
+        )
+    }
 }
--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,105 +1,121 @@
-import { Component, Fragment } from "react";
-import "../styles/default.css";
+import { Component, Fragment } from 'react'
+import '../styles/default.css'
 
 export class ContactMe extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <Fragment>
-        {/* ContactUs */}
-        <section id="contact">
-          <div className="row section-head">
-            <div className="two columns header-col">
-              <h1>
-                <span>Get In Touch.</span>
-              </h1>
-            </div>
-            <div className="ten columns">
-              <h2 className="lead">Contact Me</h2>
-            </div>
-          </div>
-          <div className="row">
-            <div className="eight columns">
-              {/* form */}
-              <form action method="post" id="contactForm" name="contactForm">
-                <fieldset>
-                  <div>
-                    <label htmlFor="contactName">
-                      Name <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactName"
-                      name="contactName"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="contactEmail">
-                      Email <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size={35}
-                      id="contactEmail"
-                      name="contactEmail"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="contactSubject">Subject</label>
-                    <input
-                      type="text"
-                      defaultValue="Contact Me: More Info"
-                      size={35}
-                      id="contactSubject"
-                      name="contactSubject"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="contactMessage">
-                      Message <span className="required">*</span>
-                    </label>
-                    <textarea
-                      cols={50}
-                      rows={15}
-                      id="contactMessage"
-                      name="contactMessage"
-                      defaultValue={""}
-                    />
-                  </div>
-                  <div>
-                    <button className="submit">Submit</button>
-                    <span id="image-loader">
-                      <img alt="" src="images/loader.gif" />
-                    </span>
-                  </div>
-                </fieldset>
-              </form>{" "}
-              {/* Form End */}
-              {/* contact-warning */}
-              <div id="message-warning"> Error boy</div>
-              {/* contact-success */}
-              <div id="message-success">
-                <i className="fa fa-check" />
-                Your message was sent, thank you!
-                <br />
-              </div>
-            </div>
-            <aside className="four columns footer-widgets">
-              <div className="widget widget_contact">
-                <h4>Address and Email</h4>
-                <p className="address">
-                  {resumeData.name}
-                  <br />
-                  {resumeData.address} <br />
-                  {resumeData.city}, {resumeData.state} {resumeData.zip} US
-                  <br />
-                  <span>{resumeData.email}</span>
-                </p>
-              </div>
-              {/*
+    render() {
+        let resumeData = this.props.resumeData
+        return (
+            <Fragment>
+                {/* ContactUs */}
+                <section id="contact">
+                    <div className="row section-head">
+                        <div className="two columns header-col">
+                            <h1>
+                                <span>Get In Touch.</span>
+                            </h1>
+                        </div>
+                        <div className="ten columns">
+                            <h2 className="lead">Contact Me</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="eight columns">
+                            {/* form */}
+                            <form
+                                action
+                                method="post"
+                                id="contactForm"
+                                name="contactForm"
+                            >
+                                <fieldset>
+                                    <div>
+                                        <label htmlFor="contactName">
+                                            Name{' '}
+                                            <span className="required">*</span>
+                                        </label>
+                                        <input
+                                            type="text"
+                                            defaultValue=""
+                                            size="35"
+                                            id="contactName"
+                                            name="contactName"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="contactEmail">
+                                            Email{' '}
+                                            <span className="required">*</span>
+                                        </label>
+                                        <input
+                                            type="text"
+                                            defaultValue=""
+                                            size={35}
+                                            id="contactEmail"
+                                            name="contactEmail"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="contactSubject">
+                                            Subject
+                                        </label>
+                                        <input
+                                            type="text"
+                                            defaultValue="Contact Me: More Info"
+                                            size={35}
+                                            id="contactSubject"
+                                            name="contactSubject"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="contactMessage">
+                                            Message{' '}
+                                            <span className="required">*</span>
+                                        </label>
+                                        <textarea
+                                            cols={50}
+                                            rows={15}
+                                            id="contactMessage"
+                                            name="contactMessage"
+                                            defaultValue={''}
+                                        />
+                                    </div>
+                                    <div>
+                                        <button className="submit">
+                                            Submit
+                                        </button>
+                                        <span id="image-loader">
+                                            <img
+                                                alt=""
+                                                src="images/loader.gif"
+                                            />
+                                        </span>
+                                    </div>
+                                </fieldset>
+                            </form>{' '}
+                            {/* Form End */}
+                            {/* contact-warning */}
+                            <div id="message-warning"> Error boy</div>
+                            {/* contact-success */}
+                            <div id="message-success">
+                                <i className="fa fa-check" />
+                                Your message was sent, thank you!
+                                <br />
+                            </div>
+                        </div>
+                        <aside className="four columns footer-widgets">
+                            <div className="widget widget_contact">
+                                <h4>Address and Email</h4>
+                                <p className="address">
+                                    {resumeData.name}
+                                    <br />
+                                    {resumeData.address} <br />
+                                    {resumeData.city}, {resumeData.state}{' '}
+                                    {resumeData.zip} US
+                                    <br />
+                                    <span>{resumeData.email}</span>
+                                </p>
+                            </div>
+                            {/*
               <div className="widget widget_tweets">
                 <h4 className="widget-title">Latest Tweets</h4>
                 <ul id="twitter">
@@ -129,11 +145,11 @@ export class ContactMe extends Component {
                 </ul>
               </div>
               */}
-            </aside>
-          </div>
-        </section>{" "}
-        {/* ContactUs End*/}
-      </Fragment>
-    );
-  }
+                        </aside>
+                    </div>
+                </section>{' '}
+                {/* ContactUs End*/}
+            </Fragment>
+        )
+    }
 }
