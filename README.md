3d9241183ca76ac543477971ad7fa6b082f22def
Jeff <jeff@cogentcoder.com>
2020-12-07 23:45:12 -0500

    Changes  to use Bootstrap 5

--- README.md
+++ README.md
@@ -68,3 +68,13 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+## Prettier CheatSheet
+
+```bash
+    # install prettier
+    yarn add --dev --exact prettier
+
+    # format all files using Prettier
+    yarn prettier --write .
+```
--- src/App.js
+++ src/App.js
@@ -5,32 +5,32 @@
   How to CSS Style in React
   https://www.robinwieruch.de/react-css-styling#css-in-css-css-modules-in-react
  */
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg'
+import './App.css'
 
-import React, { Component } from "react";
-import { Header } from "./components/Header";
-import { About } from "./components/About";
-import { Resume } from "./components/Resume";
-import { Portfolio } from "./components/Portfolio";
+import React, { Component } from 'react'
+import { Header } from './components/Header'
+import { About } from './components/About'
+import { Resume } from './components/Resume'
+import { Portfolio } from './components/Portfolio'
 // import { Testimonials } from "./components/Testimonials";
-import { ContactMe } from "./components/ContactMe";
-import { Footer } from "./components/Footer";
+import { ContactMe } from './components/ContactMe'
+import { Footer } from './components/Footer'
 
-import resumeData from "./js/resumeData";
+import resumeData from './js/resumeData'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header resumeData={resumeData} />
-        <About resumeData={resumeData} />
-        <Resume />
-        <Portfolio />
-        <ContactMe resumeData={resumeData} />
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                {/* <Header resumeData={resumeData} /> */}
+                <About resumeData={resumeData} />
+                <Resume resumeData={resumeData} />
+                {/* <Portfolio /> */}
+                {/*<ContactMe resumeData={resumeData} />*/}
+                {/*<Footer />*/}
+            </div>
+        )
+    }
 }
-export default App;
+export default App
